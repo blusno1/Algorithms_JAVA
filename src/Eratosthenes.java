739da48b541f5f8bbf63d7ebfe1c109d52fc55ba
blusno <blusno@126.com>
2016-05-11 22:06:04 +0800

    init commit

---
+++ src/Eratosthenes.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by root on 16-5-11.
+ */
+public class Eratosthenes {
+
+    public static List<Integer> cal(int m){
+        List<Integer> list = new ArrayList<>();
+        Integer a[] = new Integer[m+1];
+        for(int i=0;i<a.length;i++){
+            a[i] = i;
+        }
+        a[1] = 0;
+        for (int i=2;i<Math.sqrt(m);i++){
+            for (int j = i+1;j<=m;j++){
+                if(a[j]!=0&&a[j]%i==0){
+                    a[j]=0;
+                }
+            }
+        }
+
+        for (int i=0;i<=m;i++){
+            if (a[i]!=0){
+                list.add(a[i]);
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = cal(1000000);
+        for (int i:list){
+            System.out.println(i);
+        }
+    }
+}
